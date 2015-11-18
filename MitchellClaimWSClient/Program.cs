eb3b220cb743f2c38250b3b355757695fbf8c6bf
AnasZaakou <anaszaakouk@gmail.com>
2015-11-18 01:49:21 -0500

    Auto Claim Web Service
    Mitchell Claim Web Service is a WCF Application to provide the
    functionality of CRUD operations to manage auto claims by manipulating
    XML data.

---
+++ MitchellClaimWSClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MitchellClaimWSClient.MitchellClaimWSReference;
+
+namespace MitchellClaimWSClient
+{
+    class Program
+    {
+        static string strCreateClaimXml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <cla:MitchellClaim xmlns:cla=""http://www.mitchell.com/examples/claim"">
+                              <cla:ClaimNumber>1</cla:ClaimNumber>
+                              <cla:ClaimantFirstName>George</cla:ClaimantFirstName>
+                              <cla:ClaimantLastName>Washington</cla:ClaimantLastName>
+                              <cla:Status>OPEN</cla:Status>
+                              <cla:LossDate>2014-07-09T17:19:13.631-07:00</cla:LossDate>
+                              <cla:LossInfo>
+                                <cla:CauseOfLoss>Collision</cla:CauseOfLoss>
+                                <cla:ReportedDate>2014-07-10T17:19:13.676-07:00</cla:ReportedDate>
+                                <cla:LossDescription>Crashed into an apple tree.</cla:LossDescription>
+                              </cla:LossInfo>
+                              <cla:AssignedAdjusterID>12345</cla:AssignedAdjusterID>
+                              <cla:Vehicles>
+                                <cla:VehicleDetails>
+                                  <cla:ModelYear>2015</cla:ModelYear>
+                                  <cla:MakeDescription>Ford</cla:MakeDescription>
+                                  <cla:ModelDescription>Mustang</cla:ModelDescription>
+                                  <cla:EngineDescription>EcoBoost</cla:EngineDescription>
+                                  <cla:ExteriorColor>Deep Impact Blue</cla:ExteriorColor>
+                                  <cla:Vin>1M8GDM9AXKP042788</cla:Vin>
+                                  <cla:LicPlate>NO1PRES</cla:LicPlate>
+                                  <cla:LicPlateState>VA</cla:LicPlateState>
+                                  <cla:LicPlateExpDate>2015-03-10-07:00</cla:LicPlateExpDate>
+                                  <cla:DamageDescription>Front end smashed in. Apple dents in roof.</cla:DamageDescription>
+                                  <cla:Mileage>1776</cla:Mileage>
+                                </cla:VehicleDetails>
+                              </cla:Vehicles>
+                            </cla:MitchellClaim>";
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to Mitchell Claim WS's Client: ");
+                // Creating the reference
+                ClaimServiceClient ClaimWSClient = new ClaimServiceClient();
+                Console.WriteLine("Adding new claim with the following details:");
+                Console.WriteLine(strCreateClaimXml);
+                Console.WriteLine("Please wait while Processing your request...");
+                // Creating a new claim
+                Console.WriteLine(ClaimWSClient.CreateClaim(strCreateClaimXml)? "Claim added successfully": "Error adding claim");
+                Console.WriteLine("----------");
+                Console.WriteLine("Getting this claim from backing store with claim number ");
+                Console.WriteLine(ClaimWSClient.GetClaimByClaimNumber("1").claimNumberField);
+                Console.WriteLine("----------");
+                Console.WriteLine("Getting list of claims falling between dates 2014-07-09T17:19:13.631-07:00 and 2015-07-09T17:19:13.631-07:00 ");
+                Console.WriteLine(ClaimWSClient.GetClaimsBetweenDates(Convert.ToDateTime("2014-07-09T17:19:13.631-07:00"), Convert.ToDateTime("2015-07-09T17:19:13.631-07:00")).Length + " Claims Found");
+            }
+            catch
+            {
+                Console.WriteLine(" Error in getting data from the Claim Service");
+            }
+        }
+    }
+}
---
+++ MitchellClaimWSTest/ClaimServiceTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MitchellClaimWSTest
+{
+    /// <summary>
+    /// Testing the MitchellClaimWSLib web service 
+    /// </summary>
+    [TestClass]
+    public class ClaimServiceTest
+    {
+        string strCreateClaimXml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+                            <cla:MitchellClaim xmlns:cla=""http://www.mitchell.com/examples/claim"">
+                              <cla:ClaimNumber>1</cla:ClaimNumber>
+                              <cla:ClaimantFirstName>George</cla:ClaimantFirstName>
+                              <cla:ClaimantLastName>Washington</cla:ClaimantLastName>
+                              <cla:Status>OPEN</cla:Status>
+                              <cla:LossDate>2014-07-09T17:19:13.631-07:00</cla:LossDate>
+                              <cla:LossInfo>
+                                <cla:CauseOfLoss>Collision</cla:CauseOfLoss>
+                                <cla:ReportedDate>2014-07-10T17:19:13.676-07:00</cla:ReportedDate>
+                                <cla:LossDescription>Crashed into an apple tree.</cla:LossDescription>
+                              </cla:LossInfo>
+                              <cla:AssignedAdjusterID>12345</cla:AssignedAdjusterID>
+                              <cla:Vehicles>
+                                <cla:VehicleDetails>
+                                  <cla:ModelYear>2015</cla:ModelYear>
+                                  <cla:MakeDescription>Ford</cla:MakeDescription>
+                                  <cla:ModelDescription>Mustang</cla:ModelDescription>
+                                  <cla:EngineDescription>EcoBoost</cla:EngineDescription>
+                                  <cla:ExteriorColor>Deep Impact Blue</cla:ExteriorColor>
+                                  <cla:Vin>1M8GDM9AXKP042788</cla:Vin>
+                                  <cla:LicPlate>NO1PRES</cla:LicPlate>
+                                  <cla:LicPlateState>VA</cla:LicPlateState>
+                                  <cla:LicPlateExpDate>2015-03-10-07:00</cla:LicPlateExpDate>
+                                  <cla:DamageDescription>Front end smashed in. Apple dents in roof.</cla:DamageDescription>
+                                  <cla:Mileage>1776</cla:Mileage>
+                                </cla:VehicleDetails>
+                              </cla:Vehicles>
+                            </cla:MitchellClaim>";
+
+        string strCreateClaim_CorruptedXml = "<notvalid></notvalid>";
+
+        // MitchellClaimWSLib object
+        static readonly MitchellClaimWSReference.ClaimServiceClient claimService = new MitchellClaimWSReference.ClaimServiceClient();
+
+        /// <summary>
+        /// Testing to add a corrupted xml
+        /// Expected result is false
+        /// </summary>
+        [TestMethod]
+        public void testCreateClaimCorruptedXMLfile()
+        {
+            Assert.IsFalse(claimService.CreateClaim(strCreateClaim_CorruptedXml));
+        }
+
+        /// <summary>
+        /// Testing to add a valid xml
+        /// Expected result is true
+        /// </summary>
+        [TestMethod]
+        public void testCreateClaimCorrectXMLfile()
+        {
+            Assert.IsTrue(claimService.CreateClaim(strCreateClaimXml));
+        }
+
+        /// <summary>
+        /// Testing to get claim by claim number with input as empty string
+        /// Expected result is null
+        /// </summary>
+        [TestMethod]
+        public void testGetClaimByClaimNumberEmptyString()
+        {
+            Assert.IsNull(claimService.GetClaimByClaimNumber(string.Empty));
+        }
+
+        /// <summary>
+        /// Testing to get a claim by claim number with input as null string
+        /// Expected result is false
+        /// </summary>
+        [TestMethod]
+        public void testGetClaimByClaimNumberNullString()
+        {
+            Assert.IsNull(claimService.GetClaimByClaimNumber(null));
+        }
+
+        /// <summary>
+        /// Testing to get an existing claim by claim number  
+        /// Expected result claim numbers are equal
+        /// </summary>
+        [TestMethod]
+        public void testGetClaimByClaimNumberClaimExists()
+        {
+            claimService.CreateClaim(strCreateClaimXml);
+            Assert.AreEqual("1", claimService.GetClaimByClaimNumber("1").claimNumberField);
+        }
+
+        /// <summary>
+        /// Testing to get claims that are not in the backing store
+        /// Expected result is zero as the the number of elements of the returned list
+        /// </summary>
+        [TestMethod]
+        public void testGetGetClaimsBetweenFutureDates()
+        {
+            Assert.AreEqual(0, claimService.GetClaimsBetweenDates(DateTime.Now.AddYears(100), DateTime.Now.AddYears(100)).Length);
+        }
+
+        /// <summary>
+        /// Testing to get existing claims in the backing store that fall between the input two dates
+        /// Expected result is not null to indicate a list that has at least one element
+        /// </summary>
+        [TestMethod]
+        public void testGetGetClaimsBetweenDatesClaimExists()
+        {
+            claimService.CreateClaim(strCreateClaimXml);
+            Assert.IsNotNull(claimService.GetClaimsBetweenDates(DateTime.Parse("2013-07-09T17:19:13.631-07:00")
+                , DateTime.Parse("2015-07-09T17:19:13.631-07:00"))[0]);
+        }
+    }
+}
